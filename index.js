6d85deb6f76d60e2a9cb099007aa926a9f6e7236
Rick Companje <rick@companje.nl>
2020-04-09 16:29:46 +0200

    partly implementing RiC-O

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 //usage: $ ./index.js INPUT.txt
+
 const fs = require('fs');
 const N3 = require('n3');
 const { DataFactory } = N3;
@@ -10,13 +11,17 @@ if (!fs.existsSync(filename)) return console.log('File not found: ' + filename);
 
 const stream = fs.createReadStream(filename); //, {encoding: 'utf8'}),
 const jsonParser = require('JSONStream').parse('*');
+var prevItem;
 
 const writer = new N3.Writer(process.stdout, { end: false, prefixes: { 
   rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
   rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
   aio: 'https://archief.io/id/',
   soort: 'https://archief.io/soort#',
-  v: 'https://archief.io/veld#'}
+  v: 'https://archief.io/veld#',
+  rico: 'https://www.ica.org/standards/RiC/ontology#',
+  recordType: 'https://www.ica.org/standards/RiC/vocabularies/recordSetTypes#'
+  }
 });
 
 stream.pipe(jsonParser);
@@ -26,21 +31,32 @@ jsonParser.on('data', function (item) { //each object
   if (!item.GUID) return console.error("Error: Skip item: GUID undefined",item.id || item);
   if (!item.aet) console.warn("Warning: AET undefined",item.id || item);
 
+  // Structuur: https://docs.google.com/drawings/d/1IecmyErKqgqg2S9uEk3ThlvmN8DQ3lR3F_ztq_xsbM8/edit
   const subject = namedNode(`aio:${item.GUID}`);
 
-  if (item.aet) writer.addQuad(subject, namedNode('rdf:type'), namedNode(`soort:${item.aet}`));
-  writer.addQuad(subject, namedNode('rdfs:label'), literal(item.na || "?"));
-
-  if (item.parentItem) writer.addQuad(subject, namedNode('v:parent'), namedNode(`aio:${item.parentItem}`));
+  if (prevItem && prevItem.parentItem == item.parentItem) {
+    writer.addQuad(subject, namedNode('rico:follows'), namedNode(`aio:${prevItem.GUID}`));
+  }
+  
+  //rico:recordResourceExtent
 
   for (const veld in item) {
     if (!item[veld]) continue; //value undefined or empty
-    if (veld=="parentItem") continue; //already present as v:parent
-    if (veld=="na") continue; //already present as rdfs:label
-    if (veld=="aet") continue; //already present as rdf:type soort:...
-    if (veld=="GUID") continue; //already present as part of the URI
-    writer.addQuad(subject, namedNode('v:'+veld.replace(/ /g,"_").replace(/\//g,"_")), literal(item[veld]));
-  }  
+    else if (veld=="GUID") continue; //already present as part of the URI
+    else if (veld=="parentItem") writer.addQuad(subject, namedNode('rico:includedIn'), namedNode(`aio:${item.parentItem}`));
+    
+    //else if (item.aet=="abk" && veld=="ov") continue; 
+    else if (veld=="na") writer.addQuad(subject, namedNode('rico:title'), literal(item[veld]));
+    else if (veld=="pe") writer.addQuad(subject, namedNode('rico:date'), literal(item[veld])); //titel in Mais Flexis: "Datering". in uitvoer: "pe" (periode ?)
+    else if (veld=="code") writer.addQuad(subject, namedNode('rico:identifier'), literal(item[veld]));
+    else if (veld=="ov") writer.addQuad(subject, namedNode('rico:recordResourceExtent'), literal(item["ov"])); //instantiationExtent
+    
+    //TODO Instantiaten. (maar is dat niet te veel intepretatie?)
+    
+    else writer.addQuad(subject, namedNode('v:'+veld.replace(/ /g,"_").replace(/\//g,"_")), literal(item[veld]));
+  }
+ 
+  prevItem = item;
 });
 
 jsonParser.on('end', function() { //end of file
