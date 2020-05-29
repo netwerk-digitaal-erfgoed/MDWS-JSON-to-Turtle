bc747c7de448e7a5672ae11bb24678bd1a9562dd
Rick Companje <rick@companje.nl>
2020-05-29 12:48:01 +0200

    Eerste stappen van Literals naar URI

--- index.js
+++ index.js
@@ -14,31 +14,70 @@ const jsonParser = require('JSONStream').parse('*');
 
 const aiobase = 'https://archief.io/';
 
-const writer = new N3.Writer(process.stdout, { end: false, prefixes: { 
+// const adt_id = '39'
+// to uri-fi id's and ahd_id's we need to bring in adt_id to distinguish
+// records with equal id's between different archives.
+
+let prefixes = { 
+  aio: aiobase+'id/',
+  dct: 'http://purl.org/dc/terms/',
   rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
   rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
-  dct: 'http://purl.org/dc/terms/',
-  aio: aiobase+'id/',
+  recordType: 'https://www.ica.org/standards/RiC/vocabularies/recordSetTypes#',
+  rico: 'https://www.ica.org/standards/RiC/ontology#',
   soort: aiobase+'soort#',
   v: aiobase+'veld#',
-  rico: 'https://www.ica.org/standards/RiC/ontology#',
-  recordType: 'https://www.ica.org/standards/RiC/vocabularies/recordSetTypes#'
-  }
+  xsd: 'http://www.w3.org/2001/XMLSchema#'
+}
+
+const uriFields = [
+  "Auteursrechthouder",
+  "Bestand_downloadbaar",
+  "bno",
+  "CATTWD",
+  "CXTWD_PLAATSNAAM",
+  "CXTWD_STRAATNAAM",
+  "CXTWD_VERVAARDIGER",
+  "Deelcollectie",
+  "fnc_bestand_intern",
+  "fnc_eigendomein",
+  "fnc_lic",
+  "fnc_vrij_downloaden",
+  "Materiaal",
+  "Materiaalsoort",
+  "Soort_beeldmateriaal",
+  "THTWD",
+  "THWTW",
+  "Uitleg_Auteursrechten",
+  "Vervaardiger",
+  ];
+
+for (const uriField of uriFields) {
+  prefixes[uriField] = aiobase+'def/'+uriField+"#";
+}
+
+const writer = new N3.Writer(process.stdout, { end: false, prefixes
 });
 
 stream.pipe(jsonParser);
 
 jsonParser.on('data', function (item) { //each object
+
+  for (let veld in item) {    
+    safe_veld = veld.replace(/[ \/#]/g,"_");
+    if (veld!=safe_veld) {
+      item[safe_veld] = item[veld];
+      delete item[veld];
+    }
+  }
+
   if (typeof(item)!="object") { console.error("Error: Array expected as root of JSON file"); process.exit(1); };
   if (!item.GUID) return console.error("Error: Skip item: GUID undefined",item.id || item);
   if (!item.aet) console.warn("Warning: AET undefined",item.id || item);
 
-  // Structuur: https://docs.google.com/drawings/d/1IecmyErKqgqg2S9uEk3ThlvmN8DQ3lR3F_ztq_xsbM8/edit
   const subject = namedNode(`aio:${item.GUID}`);
   
-  //rico:recordResourceExtent
-
-  for (const veld in item) {
+  for (let veld in item) {
     if (!item[veld]) continue; //value undefined or empty
 
     //handle both arrays and single values
@@ -48,10 +87,16 @@ jsonParser.on('data', function (item) { //each object
 
       if (veld=="relaties") {
         if (value.rel_stuk_aet_code!="PAP") { //relatie naar fysieke stukken niet relevant)
-          // console.warn(value);
           const rel_aet = (value.rel_stuk_aet_code || value.rel_top_aet_code).toLowerCase();
           writer.addQuad(subject, namedNode('dct:relation'), namedNode(`${aiobase}id/${value.rel_adt_id}/${rel_aet}/${value.ahd_id_rel}`));
         }
+      }
+
+      else if (uriFields.indexOf(veld)>-1) {
+        //Make safe URI field and value
+        //Google Sheets formula: // REGEXREPLACE(REGEXREPLACE(REGEXREPLACE((REGEXREPLACE(B92, "[|, ""$/']", "-")), "[().]", "") ,"--","-"),"--","-")
+        let uri = value.replace(/[:;|,<>=& "$/']/g, "-").replace(/[().]/g, "").replace(/--/g,"-").replace(/--/g,"-");
+        writer.addQuad(subject, namedNode('v:'+veld),namedNode(veld+":"+uri));
       }
 
       else if (veld=="GUID") continue; //already present as part of the URI
@@ -59,14 +104,17 @@ jsonParser.on('data', function (item) { //each object
       else if (veld=="previousItem") writer.addQuad(subject, namedNode('rico:follows'), namedNode(`aio:${item.previousItem}`));
       else if (veld=="aet") writer.addQuad(subject, namedNode('v:aet'), namedNode(`soort:${item.aet}`));
 
-      //else if (item.aet=="abk" && veld=="ov") continue; 
+      //type = date
+      else if (veld=="bj" || veld=="ej") writer.addQuad(subject, namedNode("v:"+veld), literal(value,{value:"http://www.w3.org/2001/XMLSchema#date"}));
+
+      //RiC-O
       else if (veld=="na") writer.addQuad(subject, namedNode('rico:title'), literal(value));
       else if (veld=="pe") writer.addQuad(subject, namedNode('rico:date'), literal(value)); //titel in Mais Flexis: "Datering". in uitvoer: "pe" (periode ?)
       else if (veld=="code") writer.addQuad(subject, namedNode('rico:identifier'), literal(value));
       else if (veld=="ov") writer.addQuad(subject, namedNode('rico:recordResourceExtent'), literal(item["ov"])); //instantiationExtent
     
-      //default
-      else writer.addQuad(subject, namedNode('v:'+veld.replace(/[ \/#]/g,"_")), literal(value));
+      //default literals
+      else writer.addQuad(subject, namedNode('v:'+veld), literal(value));
     }
   }
 });
